2e0892e5bb42e6285432df64388eacc757be6837
luobata <batayao@sohu-inc.com>
2017-10-30 20:23:45 +0800

    upd8

--- track/track-1.js
+++ track/track-1.js
@@ -2,15 +2,37 @@
  * @desctiption 
  */
 
-const track = 'res.data.code';
+const track = 'data.code';
 const res = {
     data: {
         code: 200
     }
 };
+const data = 404;
 
-const trackFunctory = () => {
+const trackFunctory = (res, track, data) => {
+    const splitKeys = (key, vNode) => {
+        const keyArr = key.split('.');
+        let name = vNode;
+        let str = 'that';
+        for (let i of keyArr) {
+            name = name[i];
+            str += "['" + i + "']";
+        }
 
+        return {
+            funStr: str,
+            value: name
+        };
+    };
+
+    const strObj = splitKeys(track, res);
+    const name = data;
+    //const name = strObj.value.replace(/[^\d]/g, '').replace(/^0$/, '');
+    const str = strObj.funStr + '=' + '"' + name + '"';
+
+    new Function('that', str)(res);
 };
 
-trackFunctory(res, track); // 200 and it's easy
+trackFunctory(res, track, data); // change and it's easy
+console.log(res);
